2ba96b3ed31eafd672836335c20571e67386b853
Leander List <list.leander@gmail.com>
2025-03-03 10:41:11 +0100

    implementt header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <header>
-    
+    <Header/>
   </header>
   <main>
-
   </main>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="flex w-full h-20 fixed">
+        <div class="flex items-center justify-between px-3 my-2 mx-4 w-full h-8 border-2 border-slate-500 bg-base-100 rounded-full"
+            style="filter: brightness(1.3);">
+            <div class="flex items-center">
+                <ul class="menu menu-sm !p-0 menu-horizontal rounded-box bg-base-100 w-130 flex justify-between gap-2">
+                    <li class="flex-1">
+                        <details>
+                            <summary> 3. JG </summary>
+                            <ul class="w-full">
+                                <li><a>Keyframeanimation</a></li>
+                                <li><a>UV-Mapping</a></li>
+                                <li><a>Robotershader</a></li>
+                                <li><a>Shadering</a></li>
+                                <li><a>Roboter</a></li>
+                                <li><a>Tinkercad</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li class="flex-1 m-0">
+                        <details>
+                            <summary> 4. JG </summary>
+                            <ul class="w-full">
+                                <li><a>Blendshapes</a></li>
+                                <li><a>Rig</a></li>
+                                <li><a>Dynamics</a></li>
+                                <li><a>NURBS</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li class="flex-1">
+                        <details>
+                            <summary> 5. JG </summary>
+                            <ul class="w-full">
+                                <li>
+                                    <details open>
+                                        <summary>Theorie</summary>
+                                        <ul>
+                                            <li><a>Booleans</a></li>
+                                            <li><a>Entwicklungsstufen</a></li>
+                                            <li><a>nParticles</a></li>
+                                            <li><a>CGI - Ausgangsfoto</a></li>
+                                            <li><a>nCloth</a></li>
+                                        </ul>
+                                    </details>
+                                </li>
+                                <li>
+                                    <details open>
+                                        <summary>Labor</summary>
+                                        <ul>
+                                            <li><a>Liquids</a></li>
+                                            <li><a>CGI</a></li>
+                                            <li><a>xGen</a></li>
+                                        </ul>
+                                    </details>
+                                </li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <p class="text-center text-sm">3D-Showcase</p>
+            </div>
+        </div>
+    </div>
+
+</template>
