bd2a10e42d69425ab35ca827980f7c63aa3e8234
Leander List <llist@student.tgm.ac.at>
2025-03-30 22:03:22 +0200

    decrease heading size

--- src/components/ImageEntry.vue
+++ src/components/ImageEntry.vue
@@ -4,7 +4,7 @@
             <img :src="`images/${props.image}.png`" :alt="`${props.subheading} image`" class="h-100 w-full object-cover" />
         </div>
         <div class="px-10 w-full">
-            <h2 class="text-4xl">{{ props.title }}</h2>
+            <h2 class="text-3xl">{{ props.title }}</h2>
             <p class="text-xl text-gray-400 mb-3">{{ props.subheading }}</p>
             <p class="text-xl">
                 <slot></slot>
