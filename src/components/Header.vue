ffb837c8fff7ef8b964d2cb26b62e3c820eb3c00
Leander List <llist@student.tgm.ac.at>
2025-03-30 23:00:32 +0200

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,60 +3,10 @@
         <div class="flex items-center justify-between px-3 my-2 mx-4 w-full h-8 border-2 border-slate-500 bg-base-100 rounded-full"
             style="filter: brightness(1.3);">
             <div class="flex items-center">
-                <ul class="menu menu-sm !p-0 menu-horizontal rounded-box bg-base-100 w-130 flex justify-between gap-2">
-                    <li class="flex-1">
-                        <details>
-                            <summary> 3. JG </summary>
-                            <ul class="w-full">
-                                <li><a>Keyframeanimation</a></li>
-                                <li><a>UV-Mapping</a></li>
-                                <li><a>Robotershader</a></li>
-                                <li><a>Shadering</a></li>
-                                <li><a>Roboter</a></li>
-                                <li><a>Tinkercad</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li class="flex-1 m-0">
-                        <details>
-                            <summary> 4. JG </summary>
-                            <ul class="w-full">
-                                <li><a>Blendshapes</a></li>
-                                <li><a>Rig</a></li>
-                                <li><a>Dynamics</a></li>
-                                <li><a>NURBS</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li class="flex-1">
-                        <details>
-                            <summary> 5. JG </summary>
-                            <ul class="w-full">
-                                <li>
-                                    <details open>
-                                        <summary>Theorie</summary>
-                                        <ul>
-                                            <li><a>Booleans</a></li>
-                                            <li><a>Entwicklungsstufen</a></li>
-                                            <li><a>nParticles</a></li>
-                                            <li><a>CGI - Ausgangsfoto</a></li>
-                                            <li><a>nCloth</a></li>
-                                        </ul>
-                                    </details>
-                                </li>
-                                <li>
-                                    <details open>
-                                        <summary>Labor</summary>
-                                        <ul>
-                                            <li><a>Liquids</a></li>
-                                            <li><a>CGI</a></li>
-                                            <li><a>xGen</a></li>
-                                        </ul>
-                                    </details>
-                                </li>
-                            </ul>
-                        </details>
-                    </li>
+                <ul class="menu menu-horizontal bg-base-200 !p-0 menu-sm rounded-box h-fit">
+                    <li><a class="px-10" @click="">3. Jahrgang</a></li>
+                    <li><a class="px-10" @click="">4. Jahrgang</a></li>
+                    <li><a class="px-10" @click="">5. Jahrgang</a></li>
                 </ul>
             </div>
             <div>
@@ -66,3 +16,5 @@
     </div>
 
 </template>
+<script lang="ts" setup>
+</script>
