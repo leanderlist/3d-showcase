b00209bfeec260cbcc9d799b156e09d4e63e73af
Leander List <llist@student.tgm.ac.at>
2025-03-30 23:28:18 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,17 @@
         <div v-for="section, index in sections" :key="section.title">
           <h2 class="text-4xl mt-10" :ref="numberToString">{{ section.title }}</h2>
           <div v-for="entry, index in section.entries" :key="entry.title">
-            <ModelEntry :title="entry.title" :model_scaling="entry.model_scaling" :model="entry.path[0]" :subheading="entry.subheading" :direction="index % 2 === 0 ? 'right' : 'left'" v-if="entry.type === 'model'">
+            <ModelEntry :title="entry.title" :model_scaling="entry.model_scaling" :model="entry.path[0]"
+              :subheading="entry.subheading" :direction="index % 2 === 0 ? 'right' : 'left'"
+              v-if="entry.type === 'model'">
               {{ entry.description }}
             </ModelEntry>
-            <ImageEntry :title="entry.title" :image="entry.path[0]" :subheading="entry.subheading" :direction="index % 2 === 0 ? 'right' : 'left'" v-if="entry.type === 'image'">
+            <ImageEntry :title="entry.title" :images="entry.path" :subheading="entry.subheading"
+              :direction="index % 2 === 0 ? 'right' : 'left'" v-if="entry.type === 'image'">
               {{ entry.description }}
             </ImageEntry>
-            <VideoEntry :title="entry.title" :video="entry.path[0]" :subheading="entry.subheading" :direction="index % 2 === 0 ? 'right' : 'left'" v-if="entry.type === 'video'">
+            <VideoEntry :title="entry.title" :video="entry.path[0]" :subheading="entry.subheading"
+              :direction="index % 2 === 0 ? 'right' : 'left'" v-if="entry.type === 'video'">
               {{ entry.description }}
             </VideoEntry>
           </div>
@@ -33,7 +37,7 @@ import VideoEntry from './components/VideoEntry.vue';
 import Header from './components/Header.vue';
 
 import entryData from "@/assets/projects.json";
-import type { Entry, Section } from "@/types/entries";
+import type { Section } from "@/types/entries";
 import { ref } from 'vue';
 
 const sections: Section[] = entryData;
@@ -43,17 +47,18 @@ const two = ref<HTMLElement | null>(null);
 const three = ref<HTMLElement | null>(null);
 
 function scrollToHeading(heading: string) {
+  console.log(heading);
   switch (heading) {
     case "one":
-      if(one.value)
+      if (one.value)
         one.value.scrollIntoView({ behavior: "smooth" });
       break;
     case "two":
-      if(two.value)
+      if (two.value)
         two.value.scrollIntoView({ behavior: "smooth" });
       break;
     case "three":
-      if(three.value)
+      if (three.value)
         three.value.scrollIntoView({ behavior: "smooth" });
       break;
     default:
